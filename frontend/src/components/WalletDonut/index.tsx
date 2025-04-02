0918deb9f0023c2d0cba652d8c5360f56a61079d
nicolas-martin <nicolas.martin.0986@gmail.com>
2025-04-02 15:14:54 -0400

    donut view

--- frontend/src/components/WalletDonut/index.tsx
+++ frontend/src/components/WalletDonut/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { View } from "react-native";
 import { Pie, PolarChart } from "victory-native";
 import { useTheme, MD3Theme } from "react-native-paper";
 import { TokenInfo } from "../../services/api";
