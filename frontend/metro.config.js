ded5bbbb2217a4e4878b9f26c1a08cd96be7af3d
nicolas-martin <nicolas.martin.0986@gmail.com>
2025-04-08 00:07:11 -0400

    --wip--

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,7 +1,6 @@
 // Learn more https://docs.expo.dev/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
-
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
 	// Enable Hermes as the default JavaScript engine
