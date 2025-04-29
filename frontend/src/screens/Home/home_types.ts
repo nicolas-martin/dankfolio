879003bd985fc87c6d1cd5b2a25462798d71263c
nicolas-martin <nicolas.martin.0986@gmail.com>
2025-04-29 16:59:29 -0400

    -wip- search screen

--- frontend/src/screens/Home/home_types.ts
+++ frontend/src/screens/Home/home_types.ts
@@ -1,5 +1,5 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList } from '@/types/navigation';
+import { RootStackParamList } from '@/types/index';
 import { NotificationProps } from '@/types/index';
 
 export type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
