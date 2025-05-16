18357ccbef06ad2bc3864d8b6ed0ceb34105933d
nicolas-martin <nicolas.martin.0986@gmail.com>
2025-05-16 14:20:55 -0400

    add time hover label on chart

--- frontend/src/components/Chart/CoinChart/styles.ts
+++ frontend/src/components/Chart/CoinChart/styles.ts
@@ -10,4 +10,16 @@ export const createStyles = (theme: MD3Theme) => StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
+	hoverTimeText: {
+		position: 'absolute',
+		color: theme.colors.onSurfaceVariant,
+		fontSize: 12,
+		textAlign: 'center',
+	},
+	hoverLabel: {
+		position: 'absolute',
+		width: 120,
+		alignItems: 'center',
+		pointerEvents: 'none',
+	},
 });
