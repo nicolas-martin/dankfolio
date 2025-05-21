f1d3e6d5bba5db350067779c13485c5ecf631cfa
nicolas-martin <nicolas.martin.0986@gmail.com>
2025-05-21 14:44:08 -0400

    test

--- frontend/src/screens/Home/index.tsx
+++ frontend/src/screens/Home/index.tsx
@@ -61,7 +61,7 @@ const HomeScreen = () => {
 				<View style={styles.content}>
 					<View style={styles.coinsSection}>
 						<View style={styles.sectionHeader}>
-							<Text variant="titleLarge" style={styles.sectionTitle}>Available Coins</Text>
+							<Text variant="titleLarge" style={styles.sectionTitle}>Available Coins!</Text>
 							<IconButton
 								icon="refresh"
 								size={24}
