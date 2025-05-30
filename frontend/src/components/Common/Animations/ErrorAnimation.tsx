d75ab8d881ada238814a0494db0612c73d735402
nicolas-martin <nicolas.martin.0986@gmail.com>
2025-05-30 15:33:16 -0400

    download lottie animations

--- frontend/src/components/Common/Animations/ErrorAnimation.tsx
+++ frontend/src/components/Common/Animations/ErrorAnimation.tsx
@@ -2,32 +2,32 @@ import React from 'react';
 import LottieView from 'lottie-react-native';
 import { ViewStyle, StyleProp } from 'react-native';
 
-const errorAnimationSource = require('../../../../assets/lottie/error_cross.json');
+import errorAnimationSource from '../../../../assets/lottie/error_cross.json';
 
 interface ErrorAnimationProps {
-  autoPlay?: boolean;
-  loop?: boolean;
-  style?: StyleProp<ViewStyle>;
-  size?: number;
-  onAnimationFinish?: () => void;
+	autoPlay?: boolean;
+	loop?: boolean;
+	style?: StyleProp<ViewStyle>;
+	size?: number;
+	onAnimationFinish?: () => void;
 }
 
 const ErrorAnimation: React.FC<ErrorAnimationProps> = ({
-  autoPlay = true,
-  loop = false, // Typically, error animations don't loop
-  style,
-  size = 100,
-  onAnimationFinish,
+	autoPlay = true,
+	loop = false, // Typically, error animations don't loop
+	style,
+	size = 100,
+	onAnimationFinish,
 }) => {
-  return (
-    <LottieView
-      source={errorAnimationSource}
-      autoPlay={autoPlay}
-      loop={loop}
-      style={[{ width: size, height: size }, style]}
-      onAnimationFinish={onAnimationFinish}
-    />
-  );
+	return (
+		<LottieView
+			source={errorAnimationSource}
+			autoPlay={autoPlay}
+			loop={loop}
+			style={[{ width: size, height: size }, style]}
+			onAnimationFinish={onAnimationFinish}
+		/>
+	);
 };
 
 export default ErrorAnimation;
