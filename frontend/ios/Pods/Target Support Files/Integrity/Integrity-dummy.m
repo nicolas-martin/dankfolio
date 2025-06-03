b2c40bf24f1f7a602aff5cce5a6500e51a85b3f6
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-06-02 21:08:56 -0400

    pods

---
+++ frontend/ios/Pods/Target Support Files/Integrity/Integrity-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Integrity : NSObject
+@end
+@implementation PodsDummy_Integrity
+@end
---
+++ frontend/ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
@@ -0,0 +1,75 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FBReactNativeSpec/FBReactNativeSpec.h"
+#import "FBReactNativeSpecJSI.h"
+#import "RCTModulesConformingToProtocolsProvider.h"
+#import "react/renderer/components/lottiereactnative/ComponentDescriptors.h"
+#import "react/renderer/components/lottiereactnative/EventEmitters.h"
+#import "react/renderer/components/lottiereactnative/Props.h"
+#import "react/renderer/components/lottiereactnative/RCTComponentViewHelpers.h"
+#import "react/renderer/components/lottiereactnative/ShadowNodes.h"
+#import "react/renderer/components/lottiereactnative/States.h"
+#import "react/renderer/components/rngesturehandler_codegen/ComponentDescriptors.h"
+#import "react/renderer/components/rngesturehandler_codegen/EventEmitters.h"
+#import "react/renderer/components/rngesturehandler_codegen/Props.h"
+#import "react/renderer/components/rngesturehandler_codegen/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rngesturehandler_codegen/ShadowNodes.h"
+#import "react/renderer/components/rngesturehandler_codegen/States.h"
+#import "react/renderer/components/rnscreens/ComponentDescriptors.h"
+#import "react/renderer/components/rnscreens/EventEmitters.h"
+#import "react/renderer/components/rnscreens/Props.h"
+#import "react/renderer/components/rnscreens/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rnscreens/ShadowNodes.h"
+#import "react/renderer/components/rnscreens/States.h"
+#import "react/renderer/components/RNSentrySpec/ComponentDescriptors.h"
+#import "react/renderer/components/RNSentrySpec/EventEmitters.h"
+#import "react/renderer/components/RNSentrySpec/Props.h"
+#import "react/renderer/components/RNSentrySpec/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNSentrySpec/ShadowNodes.h"
+#import "react/renderer/components/RNSentrySpec/States.h"
+#import "react/renderer/components/rnskia/ComponentDescriptors.h"
+#import "react/renderer/components/rnskia/EventEmitters.h"
+#import "react/renderer/components/rnskia/Props.h"
+#import "react/renderer/components/rnskia/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rnskia/ShadowNodes.h"
+#import "react/renderer/components/rnskia/States.h"
+#import "react/renderer/components/safeareacontext/ComponentDescriptors.h"
+#import "react/renderer/components/safeareacontext/EventEmitters.h"
+#import "react/renderer/components/safeareacontext/Props.h"
+#import "react/renderer/components/safeareacontext/RCTComponentViewHelpers.h"
+#import "react/renderer/components/safeareacontext/ShadowNodes.h"
+#import "react/renderer/components/safeareacontext/States.h"
+#import "rnasyncstorage/rnasyncstorage.h"
+#import "rnasyncstorageJSI.h"
+#import "rnclipboard/rnclipboard.h"
+#import "rnclipboardJSI.h"
+#import "rngesturehandler_codegen/rngesturehandler_codegen.h"
+#import "rngesturehandler_codegenJSI.h"
+#import "RNKeychainSpec/RNKeychainSpec.h"
+#import "RNKeychainSpecJSI.h"
+#import "rnreanimated/rnreanimated.h"
+#import "rnreanimatedJSI.h"
+#import "rnscreens/rnscreens.h"
+#import "rnscreensJSI.h"
+#import "RNSentrySpec/RNSentrySpec.h"
+#import "RNSentrySpecJSI.h"
+#import "rnskia/rnskia.h"
+#import "rnskiaJSI.h"
+#import "RNVectorIconsSpec/RNVectorIconsSpec.h"
+#import "RNVectorIconsSpecJSI.h"
+#import "safeareacontext/safeareacontext.h"
+#import "safeareacontextJSI.h"
+
+FOUNDATION_EXPORT double ReactCodegenVersionNumber;
+FOUNDATION_EXPORT const unsigned char ReactCodegenVersionString[];
+
