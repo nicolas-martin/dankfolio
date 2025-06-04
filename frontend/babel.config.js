4fdb9ab22d78356377d3aa8261151bfd9df9f60b
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-06-04 11:36:06 -0400

    babel fix

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,7 +2,9 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: [
-			'babel-preset-expo',
+			['babel-preset-expo', {
+				unstable_transformImportMeta: true
+			}],
 			'@babel/preset-typescript'
 		],
 		plugins: [
