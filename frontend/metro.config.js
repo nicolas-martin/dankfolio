a48bd536a82b4096da7f393d9101a2f3bce391a4
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-06-05 09:24:03 -0400

    sentry init

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,16 +1,16 @@
 const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
-const { getDefaultConfig } = require('expo/metro-config');
-// Temporarily disabled Sentry Metro integration to prevent auto-upload build failures
-// const {
-// 	getSentryExpoConfig
-// } = require("@sentry/react-native/metro");
+const {
+	getSentryExpoConfig
+} = require("@sentry/react-native/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 // Temporarily using standard Expo config instead of Sentry config
-const config = getDefaultConfig(__dirname, {
-	// Enable Hermes as the default JavaScript engine
-	isCSSEnabled: true
-});
+// const config = getDefaultConfig(__dirname, {
+// 	// Enable Hermes as the default JavaScript engine
+// 	isCSSEnabled: true
+// });
+const config = getSentryExpoConfig(__dirname);
+
 
 // NOTE: Do i need this here since it's in the bableconfig?
 // config.resolver.extraNodeModules = {
