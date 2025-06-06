e184c92a64930701c35ad1ab3950e4984d4e60b9
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-06-05 21:11:18 -0400

    chart x axis less padding

--- frontend/src/components/Chart/CoinChart/index.tsx
+++ frontend/src/components/Chart/CoinChart/index.tsx
@@ -485,7 +485,7 @@ export default function CoinChart({
 					xKey="x"
 					yKeys={['y']}
 					domainPadding={{ top: 25 }}
-					padding={{ left: 0, right: 0, top: 0, bottom: 20 }}
+					padding={{ left: 0, right: 0, top: 0, bottom: 5 }}
 					chartPressState={[chartPress]}
 					axisOptions={{
 						font,
