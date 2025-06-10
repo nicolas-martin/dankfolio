5b3d17e5be24b1286d99a6cc435e36e5eb3645a5
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-06-10 17:45:01 -0400

    only watch frontend folder

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -2,6 +2,7 @@
 const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 // eslint-disable @typescript-eslint/oldrules/consistent-type-imports
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const {getDefaultConfig} = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 // Temporarily using standard Expo config instead of Sentry config
@@ -11,21 +12,25 @@ const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 // });
 const config = getSentryExpoConfig(__dirname);
 
+// Only watch files in the frontend directory
+config.watchFolders = [__dirname];
 
-// NOTE: Do i need this here since it's in the bableconfig?
-// config.resolver.extraNodeModules = {
-// 	'@assets': path.resolve(__dirname, 'assets')
-// };
+// Ignore backend files
+config.resolver.blockList = [
+	/backend\/.*/,
+	/\.git\/.*/,
+	/node_modules\/.*\/node_modules\/.*/,
+];
 
 // Add any custom configuration here
 config.resolver.sourceExts.push('mjs');
 config.resolver.unstable_enablePackageExports = true;
 config.resolver.unstable_conditionNames = ['browser', 'require', 'import', 'node'];
 
-// Exclude scripts directory from bundling
-config.resolver.blockList = [
-	/scripts\/.*/,
-];
+// NOTE: Do i need this here since it's in the bableconfig?
+// config.resolver.extraNodeModules = {
+// 	'@assets': path.resolve(__dirname, 'assets')
+// };
 
 // Wrap the config
 module.exports = wrapWithReanimatedMetroConfig(config);
