5df74a8f3d6f2ff835d8c915b7dce20ad3f50fbc
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-06-12 20:06:01 -0400

    fix token selector scenario

--- frontend/src/screens/Home/home_scripts.ts
+++ frontend/src/screens/Home/home_scripts.ts
@@ -1,1 +1,10 @@
 export const SOL_MINT = 'So11111111111111111111111111111111111111112';
+
+import { Coin } from '@/types';
+import { HomeScreenNavigationProp } from './home_types';
+
+export const handleCoinPress = (coin: Coin, navigation: HomeScreenNavigationProp) => {
+	navigation.navigate('CoinDetail', {
+		coin
+	});
+};
