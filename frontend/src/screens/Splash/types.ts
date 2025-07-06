e87918b0971ce1af9a956b656cc5ea5c62a5fa62
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-07-06 13:55:29 -0400

    splash

--- frontend/src/screens/Splash/types.ts
+++
@@ -1,9 +0,0 @@
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import type { RootStackParamList } from '@/types/navigation';
-
-export type SplashScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Splash'>;
-
-export interface LoadingState {
-	portfolioLoaded: boolean;
-	trendingLoaded: boolean;
-} 
